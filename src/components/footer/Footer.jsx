f639fd649d6d281e4fa482ddb285c23ccdcf8652
Annlyn <annlynanto@gmail.com>
2024-04-12 08:57:47 +0530

    footer section adde

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import "./Footer.scss";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import bg from "../../assets/footer-bg.jpg";
+import logo from "../../assets/tmovie.png";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className="logo">
+            <img src={logo} alt="" />
+            <Link to="/">MovieDb</Link>
+          </div>
+        </div>
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact Us</Link>
+            <Link to="/">Terms of Service</Link>
+            <Link to="/">About Us</Link>
+          </div>
+
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Privacy Policy</Link>
+          </div>
+
+          <div className="footer__content__menu">
+            <Link to="/">Must Watch</Link>
+            <Link to="/">Recent</Link>
+            <Link to="/">Top IMDB</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
