67fc14486be53c766360c409fe4565a8a18f8583
Annlyn <annlynanto@gmail.com>
2024-04-19 14:40:37 +0530

    footer changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
         <div className="footer__content__logo">
           <div className="logo">
             <img src={logo} alt="" />
-            <Link to="/">MovieDb</Link>
+            <Link to="/">MyMovies</Link>
           </div>
         </div>
         <div className="footer__content__menus">
